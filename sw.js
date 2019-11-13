39fd37e1d43d7eefaccdb41dc9e497e2046755de
Abdullah Alrasheed <abdullah@integritynet.biz>
2019-11-13 23:13:31 +0800

    change path

--- sw.js
+++ sw.js
@@ -3,16 +3,16 @@ self.addEventListener('install', function(event) {
 		caches.open('my-cache').then(function(cache) {
 			return cache.addAll(
 				[
-					'/static/sw.js',
-					'/',
-					'/index.html',
-					'/static/jquery-3.4.1.min.js',
-					'/static/bootstrap/js/bootstrap.min.js',
-					'/static/bootstrap/css/bootstrap.min.css',
-					'/static/img/site.webmanifest',
-					'/static/img/favicon-16x16.png',
-					'/static/img/favicon-32x32.png',
-					'/static/img/apple-touch-icon.png'
+					'/wpa/sw.js',
+					'/wpa/',
+					'/wpa/index.html',
+					'/wpa/static/jquery-3.4.1.min.js',
+					'/wpa/static/bootstrap/js/bootstrap.min.js',
+					'/wpa/static/bootstrap/css/bootstrap.min.css',
+					'/wpa/static/img/site.webmanifest',
+					'/wpa/static/img/favicon-16x16.png',
+					'/wpa/static/img/favicon-32x32.png',
+					'/wpa/static/img/apple-touch-icon.png'
 				]
 			);
 		})
